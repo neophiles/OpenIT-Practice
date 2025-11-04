9cf7b7e3aaadf66a32e10fcf1e14be028f1fb268
neophiles <mnv.tagle@gmail.com>
2025-11-04 23:31:11 +0800

    feat: setup main

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import theme from './theme/theme'
 import App from './App.jsx'
+import { ChakraProvider } from '@chakra-ui/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </StrictMode>,
 )
